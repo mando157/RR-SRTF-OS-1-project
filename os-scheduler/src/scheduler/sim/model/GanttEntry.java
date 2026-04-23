742f727650ce08e4652effb47a02f800e652ad77
Shaher Moussa <shahirbenmoussa06@gmail.com>
2026-04-23 21:04:14 +0200

    Dont Complete

--- os-scheduler/src/scheduler/sim/model/GanttEntry.java
+++ os-scheduler/src/scheduler/sim/model/GanttEntry.java
@@ -1,1 +1,30 @@
+package scheduler.sim.model;
 
+public class GanttEntry {
+    private String processId;
+    private int startTime;
+    private int endTime;
+
+    public GanttEntry(String processId, int startTime, int endTime) {
+        this.processId = processId;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public String getProcessId() {
+        return processId;
+    }
+
+    public int getStartTime() {
+        return startTime;
+    }
+
+    public int getEndTime() {
+        return endTime;
+    }
+
+    @Override
+    public String toString() {
+        return processId + " [" + startTime + " -> " + endTime + "]";
+    }
+}
