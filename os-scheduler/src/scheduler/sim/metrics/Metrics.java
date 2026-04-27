13c75c315b5e04733dd9fcb3fd6850fd1cba9341
fatmasayed35 <157058004+fatmasayed35@users.noreply.github.com>
2026-04-27 03:42:48 +0300

    Update Metrics.java

--- os-scheduler/src/scheduler/sim/metrics/Metrics.java
+++ os-scheduler/src/scheduler/sim/metrics/Metrics.java
@@ -1,1 +1,32 @@
+package scheduler.sim.metrics;
 
+import scheduler.sim.model.Process;
+import scheduler.sim.model.GanttEntry;
+import scheduler.sim.model.Result;
+import java.util.*;
+
+public class Metrics {
+
+    public static void printResults(Result result) {
+        System.out.println("\n=========== Scheduling Results ===========");
+        System.out.println("+---------+------+------+------+-------+");
+        System.out.printf("| Process | TAT  | WT   | RT   | Burst |\n");
+        System.out.println("+---------+------+------+------+-------+");
+        
+        for (Process p : result.getFinishedProcesses()) {
+            System.out.printf("|   %-4s | %4d | %4d | %4d | %5d |\n",
+                    p.getId(), 
+                    p.getTurnaroundTime(), 
+                    p.getWaitingTime(), 
+                    p.getResponseTime(),
+                    p.getBurstTime());
+        }
+        
+        System.out.println("+---------+------+------+------+-------+");
+        System.out.printf("| Average | %.2f | %.2f | %.2f |       |\n",
+                result.getAvgTurnaroundTime(),
+                result.getAvgWaitingTime(),
+                result.getAvgResponseTime());
+        System.out.println("+-----------------------------------------+");
+    }
+}
